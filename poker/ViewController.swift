d6fe5e12e94ad79091f86378a8378f48ffea6e97
eleves <eleves@mac404inf-15.local>
2017-10-11 11:12:25 -0400

    Version :: 1.0 - Create animation

--- poker/ViewController.swift
+++ poker/ViewController.swift
@@ -17,206 +17,89 @@ class ViewController: UIViewController {
     @IBOutlet weak var slot_4: UIImageView!
     @IBOutlet weak var slot_5: UIImageView!
     
-    //---
-    var card_blur_1: UIImage!
-    var card_blur_2: UIImage!
-    var card_blur_3: UIImage!
-    var card_blur_4: UIImage!
-    var card_blur_5: UIImage!
-    
-    //---
-    @IBOutlet weak var bg_1: UIView! //<<<<<<<<<<
-    @IBOutlet weak var bg_2: UIView!
-    @IBOutlet weak var bg_3: UIView!
-    @IBOutlet weak var bg_4: UIView!
-    @IBOutlet weak var bg_5: UIView!
-    
-    //---
-    @IBOutlet weak var keep_1: UILabel! //<<<<<<<<<<
-    @IBOutlet weak var keep_2: UILabel!
-    @IBOutlet weak var keep_3: UILabel!
-    @IBOutlet weak var keep_4: UILabel!
-    @IBOutlet weak var keep_5: UILabel!
-    
-    //---
-    var arrOfCardImages: [UIImage]!
-    //---
-    var arrOfSlotImageViews: [UIImageView]!
-    //---
-    var deckOfCards = [(Int, String)]() //<<<<<<<<<<
-    //---
-    var arrOfBackgrounds: [UIView]! //<<<<<<<<<<
-    //---
-    var arrOfKeepLabels: [UILabel]! //<<<<<<<<<<
-    //----------------------//----------------------
-
-    override func viewDidLoad() {
-        //---
-        super.viewDidLoad()
-      /*  //---
-        createCardObjectsFromImages()
-        //---
-        fillUpArrays()
-        //---
-        prepareAnimations(duration: 0.5,
-                          repeating: 5,
-                          cards: arrOfCardImages)
-        //---
-        stylizeSlotImageViews(radius: 10,
-                              borderWidth: 0.5,
-                              borderColor: UIColor.black.cgColor,
-                              bgColor: UIColor.yellow.cgColor)
-        //---
-        stylizeBackgroundViews(radius: 10,
-                               borderWidth: nil,
-                               borderColor: UIColor.black.cgColor,
-                               bgColor: nil) //<<<<<<<<<<
-        //---
-        createDeckOfCards() //<<<<<<<<<<
-        //---*/
-    }
-    
-    
-
-   /* //----------------------//----------------------
-    func createDeckOfCards() { //<<<<<<<<<<
-        for a in 0...3 {
-            let suits = ["d", "h", "c", "s"]
-            for b in 1...13 {
-                deckOfCards.append((b, suits[a]))
-            }
-        } }
-    //----------------------//----------------------
-    func stylizeSlotImageViews(radius r: CGFloat,
-                               borderWidth w: CGFloat,
-                               borderColor c: CGColor,
-                               bgColor g: CGColor!) {
-        for slotImageView in arrOfSlotImageViews {
-            slotImageView.clipsToBounds = true
-            slotImageView.layer.cornerRadius = r
-            slotImageView.layer.borderWidth = w
-            slotImageView.layer.borderColor = c
-            slotImageView.layer.backgroundColor = g
-        } }
-    //----------------------//----------------------
-    func stylizeBackgroundViews(radius r: CGFloat,
-                                borderWidth w: CGFloat?,
-                                borderColor c: CGColor,
-                                bgColor g: CGColor?) { //<<<<<<<<<<
-        for bgView in arrOfBackgrounds {
-            bgView.clipsToBounds = true
-            bgView.layer.cornerRadius = r
-            bgView.layer.borderWidth = w ?? 0
-            bgView.layer.borderColor = c
-            bgView.layer.backgroundColor = g
-        } }
-    //----------------------//----------------------
-    func fillUpArrays() {
-        arrOfCardImages = [card_blur_1, card_blur_2, card_blur_3, card_blur_4,
-                           card_blur_5]
-        arrOfSlotImageViews = [slot_1, slot_2, slot_3, slot_4, slot_5]
-        arrOfBackgrounds = [bg_1, bg_2, bg_3, bg_4, bg_5]
-        arrOfKeepLabels = [keep_1, keep_2, keep_3, keep_4, keep_5]
-    }
-    //----------------------//----------------------
-    func createCardObjectsFromImages() {
-        card_blur_1 = UIImage(named: "blur_1.png")
-        card_blur_2 = UIImage(named: "blur_2.png")
-        card_blur_3 = UIImage(named: "blur_3.png")
-        card_blur_4 = UIImage(named: "blur_4.png")
-        card_blur_5 = UIImage(named: "blur_4.png")
-    }
-    //----------------------//----------------------
-    func prepareAnimations(duration d: Double,
-                           repeating r: Int,
-                           cards c: [UIImage]) {
-        for slotAnimation in arrOfSlotImageViews {
-            slotAnimation.animationDuration = d
-            slotAnimation.animationRepeatCount = r
-            slotAnimation.animationImages = returnRandomBlurCards(arrBlurCards: c)
-        } }
-    //----------------------//----------------------
-    func returnRandomBlurCards(arrBlurCards: [UIImage]) -> [UIImage] {
-        var arrToReturn = [UIImage]()
-        var arrOriginal = arrBlurCards
-        for _ in 0..<arrBlurCards.count {
-            let randomIndex = Int(arc4random_uniform(UInt32(arrOriginal.count)))
-            arrToReturn.append(arrOriginal[randomIndex])
-            arrOriginal.remove(at: randomIndex)
-        }
-        return arrToReturn
-    }
-    //----------------------//----------------------
-    @IBAction func play(_ sender: UIButton) {
-        for slotAnimation in arrOfSlotImageViews {
-            if slotAnimation.layer.borderWidth != 1.0 {
-                slotAnimation.startAnimating()
-            } }
-        //---
-        Timer.scheduledTimer(timeInterval: 2.55,
-                             target: self,
-                             selector: #selector(displayRandomCards),
-                             userInfo: nil,
-                             repeats: false) //<<<<<<<<<<
-    }
-    //----------------------//----------------------
-    @objc func displayRandomCards() { //<<<<<<<<<<
-        //---
-        var h = [(Int, String)]()
-        //---
-        var originalDeck = deckOfCards
-        //---
-        for _ in 1...5 {
-            let randomIndex = Int(arc4random_uniform(UInt32(originalDeck.count)))
-            h.append(originalDeck[randomIndex])
-            originalDeck.remove(at: randomIndex)
-        }
-        //---
-        let card_1 = "\(h[0].0)\(h[0].1).png"
-        let card_2 = "\(h[1].0)\(h[1].1).png"
-        let card_3 = "\(h[2].0)\(h[2].1).png"
-        let card_4 = "\(h[3].0)\(h[3].1).png"
-        let card_5 = "\(h[4].0)\(h[4].1).png"
-        let arrOfCards = [card_1, card_2, card_3, card_4, card_5]
-        //---
-        var counter = 0
-        for slotAnimation in arrOfSlotImageViews {
-            if slotAnimation.layer.borderWidth != 1 {
-                slotAnimation.image = UIImage(named: arrOfCards[counter])
-            }
-            counter = counter + 1
-        }
-    }
-    //----------------------//----------------------
-    @IBAction func cardsToHold(_ sender: UIButton) { //<<<<<<<<<<
-        if arrOfBackgrounds[sender.tag].layer.borderWidth == 0.5 {
-            arrOfSlotImageViews[sender.tag].layer.borderWidth = 0.5
-            arrOfBackgrounds[sender.tag].layer.borderWidth = 0.0
-            arrOfBackgrounds[sender.tag].layer.backgroundColor = nil
-            arrOfKeepLabels[sender.tag].isHidden = true
-        } else {
-            arrOfSlotImageViews[sender.tag].layer.borderWidth = 1.0
-            arrOfBackgrounds[sender.tag].layer.borderWidth = 0.5
-            arrOfBackgrounds[sender.tag].layer.borderColor = UIColor.blue.cgColor
-            arrOfBackgrounds[sender.tag].layer.backgroundColor = UIColor(red: 0.0,
-                                                                         green: 0.0, blue: 1.0, alpha: 0.5).cgColor
-            arrOfKeepLabels[sender.tag].isHidden = false
-        } }
-    //----------------------//----------------------*/
     @IBOutlet weak var btStart: UIButton!
     
-    var status = 0
-    @IBAction func action(_ sender: UIButton) {
+    var arrayOfSlots = [UIImageView]()
+    var arrayOfBlurCards = [UIImage]()
+  
+    override func viewDidLoad() {
+        //---
         
-        if status == 0{
-            btStart.setBackgroundImage(UIImage(named: "button2"), for: .normal)
-            status = 1
-        }else{
-            btStart.setBackgroundImage(UIImage(named: "button"), for: .normal)
-            status = 0
-        }
+        initArrayOfSlots()
         
+        initArrayOfBlurCards()
+        
+        super.viewDidLoad()
     }
     
+    @IBAction func action(_ sender: UIButton)
+    {
+       btStart.setBackgroundImage(UIImage(named: "button2"), for: .normal)
+     //  btStart.isEnabled = false;
+      // btStart.adjustsImageWhenDisabled = false;
+        
+       prepareAnimation()
+    }
+    
+    private func prepareAnimation()
+    {
+        for index in 0..<arrayOfSlots.count
+        {
+            let delay = Double(index) * 0.3
+            UIView.animate(withDuration: 0.2, delay: TimeInterval(delay), options: .curveEaseOut, animations: {
+                self.arrayOfSlots[index].alpha = 0.5
+            }) { (true) in
+                UIView.animate(withDuration: 0.2, delay: 0.2, options: .curveEaseOut, animations: {
+                    self.arrayOfSlots[index].alpha = 1
+                }) { (true) in
+                    if index == self.arrayOfSlots.count - 1{
+                        self.btStart.setBackgroundImage(UIImage(named: "button"), for: .normal)
+                        self.startAnimation()
+                    }
+                }
+            }
+        }
+    }
+    
+    private func startAnimation()
+    {
+        for index in 0..<arrayOfSlots.count
+        {
+            arrayOfSlots[index].animationImages = retournArrayOfImages()
+            arrayOfSlots[index].animationRepeatCount = Int((index + 1))
+            arrayOfSlots[index].animationDuration = 1.0
+            arrayOfSlots[index].startAnimating()
+        }
+    }
+    
+    private func retournArrayOfImages() -> [UIImage]
+    {
+        var array = [UIImage]()
+        
+        for _ in 1..<6 {
+            let randomIndex = Int(arc4random_uniform(UInt32(arrayOfBlurCards.count)))
+            array.append(arrayOfBlurCards[randomIndex])
+        }
+        
+        return array
+    }
+    
+    private func retournImage(named: String) -> UIImage
+    {
+        return UIImage(named: named)!
+    }
+    
+    private func initArrayOfSlots()
+    {
+      arrayOfSlots = [slot_1, slot_2, slot_3, slot_4, slot_5]
+    }
+    
+    private func initArrayOfBlurCards()
+    {
+        for index in 1..<11
+        {
+            arrayOfBlurCards.append(retournImage(named: String("blur") + String(index)))
+        }
+    }
 }
 
