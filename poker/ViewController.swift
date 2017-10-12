f4863a3e65efd03ee6b3bf5f63c8ac089debaf56
eleves <eleves@mac404inf-15.local>
2017-10-12 12:08:31 -0400

    Version :: 1.0 - Insert border and restart play

--- poker/ViewController.swift
+++ poker/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var slot_4: UIImageView!
     @IBOutlet weak var slot_5: UIImageView!
     
+    @IBOutlet weak var borderSlot_1: UIImageView!
+    @IBOutlet weak var borderSlot_2: UIImageView!
+    @IBOutlet weak var borderSlot_3: UIImageView!
+    @IBOutlet weak var borderSlot_4: UIImageView!
+    @IBOutlet weak var borderSlot_5: UIImageView!
+    
     @IBOutlet weak var btStart: UIButton!
     
     var arrayOfSlots = [UIImageView]()
@@ -28,9 +34,15 @@ class ViewController: UIViewController {
     
     var playerBackground: AVAudioPlayer?
     var playerPlay: AVAudioPlayer?
+    
+    var countTurn: Int = 0
   
     override func viewDidLoad() {
         //---
+        
+        countTurn = 0;
+        
+        initAddGestures()
         
         initArrayOfSlots()
         
@@ -43,36 +55,115 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    @objc func showBorderSlot1() {
+        if countTurn > 0 {
+            borderSlot_1.isHidden = !borderSlot_1.isHidden
+            slot_1.tag = slot_1.tag * -1
+        }
+    }
+    
+    @objc func showBorderSlot2() {
+        if countTurn > 0 {
+            borderSlot_2.isHidden = !borderSlot_2.isHidden
+            slot_2.tag = slot_2.tag * -1
+        }
+    }
+    
+    @objc func showBorderSlot3() {
+        if countTurn > 0 {
+            borderSlot_3.isHidden = !borderSlot_3.isHidden
+            slot_3.tag = slot_3.tag * -1
+        }
+    }
+    
+    @objc func showBorderSlot4() {
+        if countTurn > 0 {
+            borderSlot_4.isHidden = !borderSlot_4.isHidden
+            slot_4.tag = slot_4.tag * -1
+        }
+    }
+    
+    @objc func showBorderSlot5() {
+        if countTurn > 0 {
+            borderSlot_5.isHidden = !borderSlot_5.isHidden
+            slot_5.tag = slot_5.tag * -1
+        }
+    }
+
+    @IBAction func actionRecommancer(_ sender: UIButton)
+    {
+        
+        borderSlot_1.isHidden = true
+        borderSlot_2.isHidden = true
+        borderSlot_3.isHidden = true
+        borderSlot_4.isHidden = true
+        borderSlot_5.isHidden = true
+        
+        slot_1.tag = -1
+        slot_2.tag = -1
+        slot_3.tag = -1
+        slot_4.tag = -1
+        slot_5.tag = -1
+        
+        for index in 0..<arrayOfSlots.count
+        {
+            animationFlipFromLeft(slot: arrayOfSlots[index], image: String("back")+String(index+1))
+        }
+        
+        countTurn = 0
+    }
+    
     @IBAction func action(_ sender: UIButton)
     {
-       btStart.setBackgroundImage(UIImage(named: "button2"), for: .normal)
-     //  btStart.isEnabled = false;
-      // btStart.adjustsImageWhenDisabled = false;
         
-       loading()
+            btStart.setBackgroundImage(UIImage(named: "button2"), for: .normal)
+            /*btStart.isEnabled = false;
+            btStart.adjustsImageWhenDisabled = false;*/
+        
+            countTurn += 1
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 , execute: {
+            self.btStart.setBackgroundImage(UIImage(named: "button"), for: .normal)
+      
+            if self.countTurn == 1
+            {
+                self.loading()
+            }
+            else
+            {
+                self.prepareAnimation()
+            }
+        })
+    }
+    
+    private func animationFlipFromLeft(slot: UIImageView, image: String)
+    {
+        slot.image = retournImage(named: image)
+        UIView.transition(with: slot, duration: 0.5, options: .transitionFlipFromLeft, animations: nil){ (true) in}
     }
     
     private func loading()
     {
         for index in 0..<arrayOfSlots.count
         {
-            let delay = Double(index) * 0.2
-            UIView.animate(withDuration: 0.1, delay: TimeInterval(delay), options: .curveEaseOut, animations: {
-                self.arrayOfSlots[index].alpha = 0.5
-            }) { (true) in
-                UIView.animate(withDuration: 0.1, delay: 0.2, options: .curveEaseOut, animations: {
-                    self.arrayOfSlots[index].alpha = 1
+            if arrayOfSlots[index].tag == -1
+            {
+                let delay = Double(index) * 0.2
+                UIView.animate(withDuration: 0.2, delay: TimeInterval(delay), options: .curveEaseOut, animations: {
+                    self.arrayOfSlots[index].alpha = 0.5
                 }) { (true) in
-                    if index == self.arrayOfSlots.count - 1{
-                        self.btStart.setBackgroundImage(UIImage(named: "button"), for: .normal)
-                        self.prepareAnimation()
+                    UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
+                        self.arrayOfSlots[index].alpha = 1
+                    }) { (true) in
+                        if index == self.arrayOfSlots.count - 1{
+                           // self.btStart.setBackgroundImage(UIImage(named: "button"), for: .normal)
+                            self.prepareAnimation()
+                        }
                     }
                 }
             }
         }
     }
-    
-    var timer : Timer?
     
     private func prepareAnimation()
     {
@@ -85,19 +176,21 @@ class ViewController: UIViewController {
         
         for index in 0..<arrayOfSlots.count
         {
-            arrayOfSlots[index].animationImages = retournArrayOfImages()
-            arrayOfSlots[index].animationRepeatCount = Int((index + 1))
-            arrayOfSlots[index].animationDuration = 1.4
-            arrayOfSlots[index].startAnimating()
+            if arrayOfSlots[index].tag == -1
+            {
+                arrayOfSlots[index].animationImages = retournArrayOfImages()
+                arrayOfSlots[index].animationRepeatCount = Int((index + 1))
+                arrayOfSlots[index].animationDuration = 1.4
+                arrayOfSlots[index].startAnimating()
             
-            let delay = Double(index) + 0.9
+                let delay = Double(index) + 0.9
             
-            DispatchQueue.main.asyncAfter(deadline: time + delay, execute: {
-                self.arrayOfSlots[index].stopAnimating()
-                self.arrayOfSlots[index].image = self.retournImage(named: String(arrayOfDeckSelected[index].0) + String(arrayOfDeckSelected[index].1))
-            })
+                DispatchQueue.main.asyncAfter(deadline: time + delay, execute: {
+                    self.arrayOfSlots[index].stopAnimating()
+                    self.arrayOfSlots[index].image = self.retournImage(named: String(arrayOfDeckSelected[index].0) + String(arrayOfDeckSelected[index].1))
+                })
+            }
         }
-        
     }
 
     
@@ -131,6 +224,16 @@ class ViewController: UIViewController {
     private func retournImage(named: String) -> UIImage
     {
         return UIImage(named: named)!
+    }
+    
+    
+    private func initAddGestures()
+    {
+        slot_1.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.showBorderSlot1)))
+        slot_2.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.showBorderSlot2)))
+        slot_3.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.showBorderSlot3)))
+        slot_4.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.showBorderSlot4)))
+        slot_5.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.showBorderSlot5)))
     }
     
     private func initArrayOfSlots()
